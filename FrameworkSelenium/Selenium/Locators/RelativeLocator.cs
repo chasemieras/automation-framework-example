0d543a42fd4c45660c0ec471d216bb53fbf3c661
Chase Mieras <chasemieras@gmail.com>
2025-05-29 16:48:35 -0400

    Just added not implemented method so the errors would go away.

--- FrameworkSelenium/Selenium/Locators/RelativeLocator.cs
+++ FrameworkSelenium/Selenium/Locators/RelativeLocator.cs
@@ -1,6 +1,15 @@
+using FrameworkSelenium.Enums;
+using OpenQA.Selenium;
+
 namespace FrameworkSelenium.Selenium.Locators
 {
     public class RelativeLocator : ILocator
     {
+        //todo DO THIS!!!
+        public By ToBy => throw new System.NotImplementedException();
+
+        public LocatorType Type => throw new System.NotImplementedException();
+
+        public string Value => throw new System.NotImplementedException();
     }
 }
